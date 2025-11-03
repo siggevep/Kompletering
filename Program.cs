1453d4cbf7089752b289bcaea8e339bfc07edafe
Sigge von Eggers Patron <sigge.voneggerspatr@learnet.se>
2025-11-03 10:01:24 +0100

    adding so i can read from users

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿using app;
+﻿using System.Reflection.Metadata;
+using app;
+
+
+List<Room> rooms = new List<Room>();
+List<User> users = new List<User>();
+
 
 Console.WriteLine("Hello, World!");
+
+if (File.Exists("Users.txt"))
+{
+
+    string[] lines = File.ReadAllLines("Users.txt");
+    foreach (string line in lines)
+    {
+
+        string[] data = line.Split(",");
+        users.Add(new(data[0], data[1]));
+    }
+}
+
--- obj/Debug/net8.0/Kompletering.AssemblyInfo.cs
+++ obj/Debug/net8.0/Kompletering.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kompletering")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aac6844966803491c8a6d2ecee12953951c3c8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c061fed48d56861543a2b4a1dc97c1e6299bfa4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kompletering")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kompletering")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
