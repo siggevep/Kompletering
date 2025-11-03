e81842a0e94c3a1967878780c216bbe21c6b6e8e
Sigge von Eggers Patron <sigge.voneggerspatr@learnet.se>
2025-11-03 09:35:40 +0100

    starting room class

---
+++ Rooms.cs
@@ -0,0 +1,19 @@
+namespace app;
+
+class Room
+{
+
+
+    int Roomnummer;
+
+    string User;
+
+
+    public Room(int roomnummer, string user)
+    {
+        Roomnummer = roomnummer;
+        User = user;
+    }
+
+}
+
